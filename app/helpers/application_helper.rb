59f4f04d9bce4a390ee9c67f8a81ddbf40d7a2fe
magicjascha <magicjascha@riseup.net>
2020-12-22 19:48:52 +0100

    raise errors for missing ENVs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def contentful
+    raise StandardError, 'environmentvariable CONTENTFUL_ACCESS_TOKEN is not set' unless ENV['CONTENTFUL_ACCESS_TOKEN']
+    raise StandardError, 'environmentvariable CONTENTFUL_SPACE_ID is not set' unless ENV['CONTENTFUL_SPACE_ID']
     @client ||= Contentful::Client.new(
         access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
         space: ENV['CONTENTFUL_SPACE_ID'],
